92f4772b30234efa9301203b7536031ac94b0ef3
Lorenzo Cirlig <113766707+Icarus4e@users.noreply.github.com>
2023-05-06 09:35:55 +0200

    Add files via upload

---
+++ Isogramma.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+public static class Isogramma
+{
+    public static bool Verifica(string word)
+    {
+        for (int i = 0; i < word.Length; i++)
+        {
+            for (int j = i + 1; j < word.Length; j++)
+            {
+                if (char.ToLower(word[i]) == char.ToLower(word[j]))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
